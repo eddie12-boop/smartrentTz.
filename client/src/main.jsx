351f345eee45e0ccde21312f5f8f4f434f34cadf
SmartRent Developer <dev@smartrent.co.tz>
2026-08-28 04:05:50 +0300

    Fix GitHub Pages Routing: Switch to HashRouter and relative module scripts

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import App from './App.jsx'
@@ -24,12 +24,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
       <ThemeProvider>
         <QueryClientProvider client={queryClient}>
-          <BrowserRouter>
+          <HashRouter>
             <App />
-          </BrowserRouter>
+          </HashRouter>
         </QueryClientProvider>
       </ThemeProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>,
 )
-
